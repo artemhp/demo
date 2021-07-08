6a3f8508c52075f1f972e9bac0bb0b4294237164
Artem Deikun <artem.deikun@zorallabs.com>
2021-07-08 14:10:20 +0300

    Routing and Bootstrap CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import Category from './components/Category/Category';
+import List from './components/List/List';
+import Order from './components/Order/Order';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Category />
+        </Route>
+        <Route exact path='/list'>
+          <List />
+        </Route>
+        <Route exact path='/order'>
+          <Order />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
 export default App;
