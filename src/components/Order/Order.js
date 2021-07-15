35b3eb746c91b62fe3255fb7a7533f2bbfb471af
Artem Deikun <artem.deikun@zorallabs.com>
2021-07-15 11:46:08 +0300

    Order. Static Markup

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styles from './Order.module.css';
+import { Jumbotron, Form, Button } from 'react-bootstrap';
 
-const Order = () => (
-  <div className={styles.Order}>
-    Order Component
-  </div>
-);
-
-Order.propTypes = {};
-
-Order.defaultProps = {};
+const Order = () => {
+  return (
+    <div className={styles.Order}>
+      <Jumbotron style={{ textAlign: 'center' }}>
+        <p>Lorem, ipsum dolor.</p>
+        <h1>Lorem.</h1>
+        <p>Lorem ipsum dolor sit.</p>
+      </Jumbotron>
+      <Form className='m-3'>
+        <Form.Group>
+          <Form.Label>Your Name:</Form.Label>
+          <Form.Control type='text' placeholder='Harry Potter' />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Message:</Form.Label>
+          <Form.Control as='textarea' placeholder='Hello, Harry!' rows={3} />
+        </Form.Group>
+        <Button block size='lg' variant='success' type='submit'>
+          Send
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
 export default Order;
